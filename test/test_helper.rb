42151c80128256e2bf9dcc1ff709ef7479152ab9
tenmasato <narito1015@icloud.com>
2022-01-14 00:32:10 +0000

    [Add]ログインテスト

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,5 +8,8 @@ class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
 
-  # Add more helper methods to be used by all tests here...
+  #テストユーザーがログイン中の場合trueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
