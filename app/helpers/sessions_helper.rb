c0cb00d8183250f3fbef0082c8bacf6f0430a6d4
tenmasato <narito1015@icloud.com>
2022-01-18 05:19:54 +0000

    [Add]User一覧

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,18 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  #永続セッションとしてユーザーを記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  #渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user = current_user
+  end
+
   #ユーザーのセッションを永続的にする
   def remember(user)
     cookies.permanent.signed[:user_id] = user.id
@@ -88,5 +100,16 @@ module SessionsHelper
   def log_out
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  #記憶したURL　(もしくはデフォルト値にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  #アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
